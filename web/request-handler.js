92808fc7847adb1130a55d1175b444496c32e5f0
Sean <dokko1230@gmail.com>
2014-05-07 15:50:58 -0700

    implements router responses

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,31 @@
 var path = require('path');
+var utils = require('./utils');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+// var handleGet = function()
+
+var getIndex = function(req, res) {
+  utils.getFile('./public/index.html', function(data) {
+    utils.sendResponse(res, data, 200);
+  });
 };
+
+var postSite = function(req, res) {
+
+};
+
+var methods = {
+  'GET': getIndex,
+  'POST': postSite
+};
+
+exports.handleRequest = function (req, res) {
+    var method = methods[req.method];
+    if(method){
+      method(req, res);
+    } else {
+      res.end('404 ERROR');
+    }
+};
+

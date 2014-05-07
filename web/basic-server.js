5342c61fac4f73a47a0255fb56f280abb3c61077
Sean <dokko1230@gmail.com>
2014-05-07 15:51:20 -0700

    adds simple available routes

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,9 +1,26 @@
 var http = require("http");
+var urlParser = require('url');
 var handler = require("./request-handler");
 
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
+
+var routes = {
+  '/': handler
+};
+
+var server = http.createServer(function(req, res) {
+  var url = urlParser.parse(req.url);
+  var route = routes[url.pathname];
+
+  if( route ){
+    route(req, res);
+  } else {
+    res.end('NOT FOUND 404');
+  }
+
+});
+
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
